d85454814b7519e790bcb8b227e09e81d57757f2
suloveghimirey <sulove_ghimirey@hotmail.com>
2022-02-17 08:57:57 +0545

    chp2_comp

--- flutter_basics/lib/main.dart
+++ flutter_basics/lib/main.dart
@@ -30,10 +30,35 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Awesome App"),
+        title: const Text("Awesome App"),
       ),
-      body: Container(
-        child: Center(child: Text("Hi Flutter")),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          alignment: Alignment.center,
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 5,
+                  offset: Offset(2.0, 5.0),
+                )
+              ],
+              color: Colors.teal,
+              gradient: LinearGradient(colors: [Colors.yellow, Colors.pink])),
+          child: Text(
+            "I am a box.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+        ),
       ),
     );
   }
